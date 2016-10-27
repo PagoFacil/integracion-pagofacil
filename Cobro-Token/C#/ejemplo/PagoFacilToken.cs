7854510f014e1e244b99fee6c110eac26b88a00c
Johnatan Ayala <jonathan@pagofacil.net>
2016-10-27 18:34:05 -0500

    Agregue codigo C# y proyecto completo

---
+++ Cobro-Token/C#/ejemplo/PagoFacilToken.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using PagoFacil.Token;
+
+namespace PagoFacil.Token
+{
+	public class PagofacilToken
+	{
+		private string idUsuario { get; set; }
+		private string llaveEncriptacion { get; set; }
+		public static bool modoProduccion;
+		//Endpoint´s
+		private static string WS_ALTA;
+		private static string WS_COBRO;
+		private static string WS_BAJA;
+
+
+		public PagofacilToken(string strIdUsuario, string strLlave)
+		{
+			try
+			{
+				if ("".Equals(strIdUsuario) || "".Equals(strLlave))
+				{
+					throw new Exception("Faltan Datos idUsuario o ApiSecret");
+				}
+
+				if (!modoProduccion)
+				{//Desarrollo
+					WS_ALTA = "https://www.pagofacil.net/st/public/CobroToken/Altatoken";
+					WS_BAJA = "https://www.pagofacil.net/st/public/CobroToken/Bajatoken";
+					WS_COBRO = "https://www.pagofacil.net/st/public/CobroToken/Transacciontoken";
+				}
+				else {//Produccion
+					WS_ALTA = "https://www.pagofacil.net/ws/public/CobroToken/Altatoken";
+					WS_BAJA = "https://www.pagofacil.net/ws/public/CobroToken/Bajatoken";
+					WS_COBRO = "https://www.pagofacil.net/ws/public/CobroToken/Transacciontoken";
+				}
+				idUsuario = strIdUsuario;
+				llaveEncriptacion = strLlave;
+			}
+			catch (Exception exc)
+			{
+				exc.Message.ToString();
+			}
+		}
+
+
+		public static void ambienteProduccionActivo(bool param)
+		{
+			modoProduccion = param;
+		}
+
+
+		/**
+		 * Metodo para tokenizar una TC
+		 * @param data
+		 * @return Respuesta del WS
+		 * @throws MalformedURLException
+		 * @throws IOException
+		 * @throws ProtocolException
+		 * @throws NoSuchAlgorithmException
+		 * @throws IllegalBlockSizeException
+		 * @throws InvalidKeyException
+		 * @throws BadPaddingException
+		 * @throws InvalidAlgorithmParameterException
+		 * @throws NoSuchPaddingException 
+		 */
+		public string altaToken(Dictionary<string, string> data)
+		{
+			return this.peticionWS(data, WS_ALTA);
+		}
+
+		public string cobroToken(Dictionary<string, string> data)
+		{
+			return this.peticionWS(data, WS_COBRO);
+		}
+
+		public string bajaToken(Dictionary<string, string> data)
+		{
+			return this.peticionWS(data, WS_BAJA);
+		}
+
+		/**
+		 * Recibe un obj Json con los parametros a enviar
+		 * @param paramsEncrypted
+		 * @return String Respuesta del WS
+		 * @throws ProtocolException
+		 * @throws MalformedURLException
+		 * @throws IOException
+		 */
+		private string peticionWS(Dictionary<string, string> data, string endPoint)
+		{
+			var jsonStr = JsonConvert.SerializeObject(data, Formatting.None);
+			Console.WriteLine("jsonStr: {0}\n" + jsonStr);
+
+			//Encriptacion AES 
+			var objAES = new PagoFacilAES();
+			string strEncriptada = objAES.Encrypt(jsonStr, llaveEncriptacion);
+			Console.WriteLine("strEncriptada: {0}\n" + strEncriptada);
+
+			//Peticiones al WS
+			var wb = new WebClient();
+			var dataSend = new NameValueCollection();
+			dataSend["idUsuario"] = idUsuario;
+			dataSend["data"] = strEncriptada;
+
+			//URL url = new URL(endPoint);
+			var response = wb.UploadValues(endPoint, "POST", dataSend);
+			var responseString = Encoding.Default.GetString(response);
+			Console.WriteLine("Response String: {0}\n", responseString);
+			var decodeJson = (string)JsonConvert.DeserializeObject(responseString);
+			Console.WriteLine("Response JsonDecode: {0}\n", decodeJson);
+			return decodeJson;
+		}
+
+	}
+
+}
---
+++ Cobro-Token/C#/source/PagoFacilToken/PagoFacilToken/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using PagoFacil.Token;
+
+//Implementacion
+namespace PagoFacil.Token
+{
+	class MainClass
+	{
+		//Dev
+		private static string key = "XXXXX";
+		private static string idUsuario = "XXXXX";
+		private static string idSucursal = "XXXXX";
+
+
+		public static void Main(string[] args)
+		{
+			//Activar modo produccion
+			//PagoFacilToken.ambienteProduccionActivo(true);
+
+			//Inicializar Obj PagoFacilToken
+			var objToken = new PagofacilToken(idUsuario, key);
+			//Inicializar obj AES para desencriptar la respuesta obtenida
+			var objAes = new PagoFacilAES();
+
+			////////////////////////
+			//     Alta token     //
+			////////////////////////
+			//Creando array asociativo, con parametros para AltaToken
+			var data = new Dictionary<string, string>
+			{
+				{"idSucursal", idSucursal},
+				{"idUsuario", idUsuario},
+				{"nombre", "Juan"},
+				{"apellidos", "Alvarado"},
+				{"numeroTarjeta", "XXXXX"},
+				{"cvt", "XXX"},
+				{"mesExpiracion", "XX"},
+				{"anyoExpiracion", "XX"},
+				{"email", "user@serv.com"},
+				{"telefono", "XXXXX"},
+				{"celular", "XXXXX"},
+				{"calleyNumero", "anatole france 311"},
+				{"colonia", "polanco"},
+				{"municipio", "miguel hidalgo"},
+				{"estado", "CDMX"},
+				{"cp", "11000"}
+			};
+
+			//string respAlta = objToken.altaToken(data);
+			//Console.WriteLine("RespAlta: {0}" + respAlta);
+			//Console.WriteLine("RespAlta-Decode: {0}" + objAes.Decrypt(respAlta, key));
+
+			////////////////////////
+			//     Cobro token    //
+			////////////////////////
+			//Creando array asociativo, con parametros para AltaToken
+			var dataCobro = new Dictionary<string, string>
+			{
+				{"idSucursal", idSucursal},
+				{"idUsuario", idUsuario},
+				{"token", "XXXXX"},
+				{"monto", "59.99"},
+				{"idPedido", "TestPf890a1"},
+				{"param1", "----------"}
+			};
+			//string respCobro = objToken.cobroToken(dataCobro);
+			//Console.WriteLine("RespCobro: {0}\n" + respCobro);
+			//Console.WriteLine("RespCobro-Decode: {0}\n" + objAes.Decrypt(respCobro, key));
+
+			////////////////////////
+			//   Baja de Token    //
+			////////////////////////
+			var dataBaja = new Dictionary<string, string>
+			{
+				{"idSucursal", idSucursal},
+				{"idUsuario", idUsuario},
+				{"token", "XXXXX"},
+				{"autorizacion", "XXXXXX"}
+			};
+
+			string respBaja = objToken.bajaToken(dataBaja);
+			Console.WriteLine("RespBaja: " + respBaja);
+			Console.WriteLine("RespBaja-Decode: " + objAes.Decrypt(respBaja, key));
+		}//End Main
+	}//End Class
+}
