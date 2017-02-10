d637656f7cfaf8920cfc6c38b89151cf64e3a1a6
PedroCruz <pedro.cruz@pagofacil.net>
2017-02-10 17:58:45 -0600

    Ejemplo para 3d secure

---
+++ 3D-Secure/PHP/ejemplo/ejemplo.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../source/PagoFacil_Descifrado_Descifrar.php';
+
+// Respuesta del web service y enviada a la url de redirección
+$respuestaWS = 'nu+81UQjTbn6oW6KFGS+UUDwC+9DtL0v0LsWVRQvBQ9Rn5BCfKB94D0ibJlUzs6ZOxkBRY1P/od7PkEM59E71DAa30viidJSUZPpYanhA7FP4D1a+toTdJlrYnaXSQ6vKfjw6k8Qel+v4qzNYghYl582LhnBpS6myPARiJk9JJzmN0BWScPSHcc9U3GEo47BwJrmFAEG3avaqlnCJYcPsPXwizGB7H4W/mxZQbxcf+CothvT8L3lM8SHGTasB7uz9eiZISxe/dnzbxDJecg7r+einaFkCdF/gIjJ8/tfyf9yl5XCJk/BRTIltbPlTszbDK/TJRSU0QZPgo2ErcyuQ0fmaFNycb4igrGEAhmnXABXRBiHHiY9XQSL7rb5lD2Pk4sEUgJkjC+Bew/ueC3VBj6TnHlQ1SdMLllUOthXzEkioMqLGLH93dRr1wJ34L6QDP0mQtlDX1zpTzNdGamqmlyMnkpOGwiYyPUc2RZWOD1zmEHy/gbDYwCJQu2mRete2EBvvsAqH+QXVwDsF8mRrpC2p7Nh2aMTKvYkaX8VcifpfOcfuC0DmvFI/gHsUNHKmKiOAxM9njrBlJ+3/IhJKvqI5A5BkJ+j8jsnI79j8UHkEpIL4hcY2T+4wLs6D9c8tnkMbcnbgny+/kfdk7owXg==';
+
+// Proporcionada por Pago Facil al momento de registrarse
+$apiKey = '123123123123';
+
+$objDesencriptar = new PagoFacil_Descifrado_Descifrar();
+
+$serializedResponse = $objDesencriptar->desencriptar($respuestaWS, $apiKey);
+
+$response = unserialize($serializedResponse);
+
+echo '<pre>';
+var_dump($response);
+echo '</pre>';
---
+++ 3D-Secure/PHP/source/PagoFacil_Descifrado_Descifrar.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Clase para descifrar contenido en PHP
+ */
+class PagoFacil_Descifrado_Descifrar
+{
+    public static function desencriptar($encodedInitialData, $key)
+    {
+        $encodedInitialData = base64_decode($encodedInitialData);
+        if (strlen($encodedInitialData) < 16) {
+            $encodedInitialData = base64_encode($encodedInitialData);
+            $encodedInitialData = $encodedInitialData . $encodedInitialData;
+            $encodedInitialData = base64_decode($encodedInitialData);
+        }
+        $iv = substr($encodedInitialData, 0, 16);
+        if (strlen($iv) < 16) {
+            $iv = str_pad($iv, 16, '0');
+        }
+        $encodedInitialData = mb_substr($encodedInitialData, 16);
+        $cypher = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+        if (mcrypt_generic_init($cypher, self::hexToBin($key), $iv) != -1) {
+            $decrypted = mdecrypt_generic($cypher, $encodedInitialData);
+            mcrypt_generic_deinit($cypher);
+            mcrypt_module_close($cypher);
+            return self::pkcs5_unpad($decrypted);
+        }
+        return "";
+    }
+
+    /**
+     * This class uses by default hex2bin function, but it is only available since 5.4, because the current php version
+     * is 5.2 this function was created with the purpose to replace the default function.
+     * @param $hex_string
+     * @return string
+     */
+    private static function hexToBin($hex_string)
+    {
+        $pos = 0;
+        $result = '';
+        while ($pos < strlen($hex_string)) {
+            if (strpos(" \t\n\r", $hex_string{$pos}) !== FALSE) {
+                $pos++;
+            } else {
+                $code = hexdec(substr($hex_string, $pos, 2));
+                $pos = $pos + 2;
+                $result .= chr($code);
+            }
+        }
+        return $result;
+    }
+
+    private static function pkcs5_unpad($text)
+    {
+        $pad = ord($text{strlen($text) - 1});
+        if ($pad > strlen($text))
+            return false;
+        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad)
+            return false;
+        return substr($text, 0, -1 * $pad);
+    }
+}
